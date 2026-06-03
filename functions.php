37f197b9fa0c08b1e1524cc7f71e5f1f3993e5e6
Jason Lawless <jasonalawless@gmail.com>
2026-06-03 19:57:05 -0400

    Update subpage header to use site-logo block with dynamic home link

--- functions.php
+++ functions.php
@@ -173,3 +173,26 @@ if ( ! function_exists( 'paradis_flooring_codex_gallery_links' ) ) :
 	}
 endif;
 add_action( 'wp_enqueue_scripts', 'paradis_flooring_codex_gallery_links' );
+
+if ( ! function_exists( 'paradis_flooring_codex_filter_site_title' ) ) :
+	/**
+	 * Filter core/site-title to render "Paradis" instead of the full site title on inner pages.
+	 *
+	 * @param string $block_content The block content HTML.
+	 * @param array  $block         The block details.
+	 * @return string               Filtered block content HTML.
+	 */
+	function paradis_flooring_codex_filter_site_title( $block_content, $block ) {
+		$classes = isset( $block['attrs']['className'] ) ? $block['attrs']['className'] : '';
+		if ( strpos( $classes, 'pfc-inner-logo' ) !== false || strpos( $classes, 'pfc-logo-main' ) !== false ) {
+			if ( preg_match( '/<a[^>]*>/i', $block_content ) ) {
+				$block_content = preg_replace( '/(<a[^>]*>).*?(<\/a>)/is', '$1Paradis$2', $block_content );
+			} else {
+				$block_content = preg_replace( '/(<[^>]+>).*?(<\/[^>]+>)/is', '$1Paradis$2', $block_content );
+			}
+		}
+		return $block_content;
+	}
+endif;
+add_filter( 'render_block_core/site-title', 'paradis_flooring_codex_filter_site_title', 10, 2 );
+
